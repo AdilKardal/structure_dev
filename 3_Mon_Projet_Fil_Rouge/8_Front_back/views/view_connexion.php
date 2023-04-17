dd94b5b322a5892c80fb85b1c5871f64df8ea712
Adil <adil.kardal@gmail.com>
2023-04-17 14:25:10 +0200

    fin jspr

--- 3_Mon_Projet_Fil_Rouge/8_Front_back/views/view_connexion.php
+++ 3_Mon_Projet_Fil_Rouge/8_Front_back/views/view_connexion.php
@@ -27,24 +27,6 @@
           <input type="submit" value="Se connecter" id="btnconnect">
         </form>
       </article>
-    
-    <article class="forminscription">
-      <h2>Pas encore inscrit ? Inscrivez-vous</h2>
-        <form action="../controllers/controller_inscription.php" method="post" enctype="multipart/form-data">
-          <input type="hidden" name="form_inscription" value="1">
-          <input type="text" name="form_nom" placeholder="Nom">
-          <input type="text" name="form_prenom" placeholder="Prenom">
-          <input type="text" name="form_email" placeholder="Mail">
-          <input type="password" name="form_mdp" placeholder="Mot de passe">
-          <input type="text" name="form_num_adresse" placeholder="15">
-          <input type="text" name="form_nom_adresse" placeholder="Rue claude monet">
-          <input type="text" name="form_cp" placeholder="Code Postal">
-          <input type="text" name="form_ville" placeholder="Ville">
-          <input type="text" name="form_pays" placeholder="france">
-          <input type="submit" value="S'inscrire">
-        </form>
-    </article>
-    
   </section>
 <?php require("view_footer.php") ?>
 
